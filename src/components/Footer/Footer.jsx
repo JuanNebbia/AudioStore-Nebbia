e90d9636fe25e85725aba57065b0be27ab8c46aa
Juan Nebbia <juan.nebbia@gmail.com>
2022-08-22 13:53:21 -0300

    agradecimientos en footer, correccion de hover

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,6 +8,9 @@ const Footer = () => {
         <p className='footer-text'>Desarrollado por Juan Nebbia en el como trabajo final del curso 
           React Js en <a href='https://coderhouse.com' target='!BLANK'>Coderhouse</a>, 
           Agosto 2022</p>
+        <p className='footer-text'>Agradecimientos a Mateo Andreassi por la mano con el diseño, al profesor Hernan Condori, 
+          la tutora Laura Therisod y mis compañeros de cursada. 
+        </p>
         <div className='footer-links'>
         <Link to={'/about'}>Sobre nosotros</Link>
         <p style={{display:'inline'}}> - </p>
